5eda95edd7df20679311fbdae0cacbcbf3321a69
devdany <dayong7387@gmail.com>
2023-07-23 22:31:31 +0900

    fix: alice img

--- src/components/ChatProfile.tsx
+++ src/components/ChatProfile.tsx
@@ -3,14 +3,15 @@ import styled from 'styled-components';
 
 const ChatProfile: React.FC = () => {
   return (
-    <ChatProfileImage src="https://cdn-api.elice.io/api/file/5064c6bd5ffc458c86a6befe8fc5136b/%E1%84%8C%E1%85%A1%E1%84%89%E1%85%A1%E1%86%AB%201.png?se=2023-07-06T00%3A15%3A00Z&sp=r&sv=2021-12-02&sr=b&sig=tMJFQl%2BIdvn9r4cILzgtxrfQOyqHK1UZb9g6rtQb/Ac%3D" />
+    <ChatProfileImage  />
   )
 }
 
 export default ChatProfile;
 
-const ChatProfileImage = styled.img`
+const ChatProfileImage = styled.div`
   width: 50px;
   height: 50px;
   border-radius: 15px;
+  background-color: lightgray;
 `
